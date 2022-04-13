9a8a94c99457b5bb58622ab51835a5ec62587dd9
Leonardo Gonçalves <leonard296@live.com>
2022-04-13 15:00:12 -0300

    change to expansion accordion menu

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,28 @@
 <template>
   <q-page>
 
-    <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-      <div v-for="cryptoData in cryptosDatas" :key="cryptoData.id" class="relative bg-white shadow-xl ring-1 mb-10 ring-gray-900/5 mx-auto w-4/5 md:w-3/5 lg:w-2/5 rounded-lg px-4 lg:px-10">
-        <div class="py-8 text-base text-gray-600">
-          <p class="grid sm:flex items-baseline">{{ cryptoData.name }} ({{ cryptoData.symbol }}) price: <code class="text-xs sm:text-sm font-bold text-gray-800 sm:pl-3 whitespace-nowrap">U$ {{ cryptoData.market_data.current_price.usd }}</code></p>
-          <p class="grid sm:flex items-baseline">Last updated: <code class="text-xs sm:text-sm font-bold text-gray-800 sm:pl-3 whitespace-nowrap">{{ cryptoData.last_updated }}</code></p>
-        </div>
-      </div>
+    <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50">
+      <q-expansion-item group="somegroup" v-for="cryptoData in cryptosDatas" :key="cryptoData.id" @show="clearWhenExpand()" class="relative bg-white shadow-xl ring-1 ring-gray-900/5 my-5 mx-auto w-4/5 md:w-3/5 lg:w-2/5 rounded-lg px-4 lg:px-10 lg:py-5">
+
+        <template v-slot:header>
+          <q-item-section class="text-base text-gray-600">
+            <p class="grid sm:flex items-baseline">{{ cryptoData.name }} ({{ cryptoData.symbol }}) price: <code class="text-xs sm:text-sm font-bold text-gray-800 sm:pl-3 whitespace-nowrap">U$ {{ cryptoData.market_data.current_price.usd }}</code></p>
+            <p class="grid sm:flex items-baseline">Last updated: <code class="text-xs sm:text-sm font-bold text-gray-800 sm:pl-3 whitespace-nowrap">{{ cryptoData.last_updated }}</code></p>
+          </q-item-section>
+        </template>
+
+        <q-card>
+          <q-card-section>
+            <p>Insira uma data</p>
+            <div class="grid sm:flex sm:flex-nowrap">
+              <q-input dense autofocus outlined clearable color="black" text-color="black" class="full-width" placeholder="31-12-2021" v-model="cryptosDatas.historyPrice" mask="##-##-####" @update:model-value="changeToNull()" />
+              <q-btn :disable="!cryptosDatas.historyPrice || cryptosDatas.historyPrice.length < 10" label="Pesquisar" class="mt-4 ml-0 sm:mt-0 sm:ml-4 bg-gray-800 text-white text-capitalize" @click="onGetHistoryCryptoPrice(cryptoData.name, cryptosDatas.historyPrice)" />
+            </div>
+            <p class="mt-4" v-if="price">O valor do(a) <code class="text-xs sm:text-sm font-bold text-gray-800">{{ cryptoData.name }}</code> na data <code class="text-xs sm:text-sm font-bold text-gray-800 whitespace-nowrap">{{ cryptosDatas.historyPrice }}</code> era de <code class="text-xs sm:text-sm font-bold text-gray-800 whitespace-nowrap">U$ {{ price }}</code></p>
+          </q-card-section>
+        </q-card>
+
+      </q-expansion-item>
     </div>
 
   </q-page>
@@ -22,7 +37,8 @@ export default {
     return {
       cryptosDatas: [],
       ticker: ['bitcoin', 'ethereum', 'dacxi' , 'terra-luna', 'cosmos'],
-      url: []
+      url: [],
+      price: null
     }
   },
 
@@ -50,11 +66,35 @@ export default {
         .catch((error) => {
           console.log(error)
         })
-        // .finally(() => {
-        //   setTimeout(() => {
-        //     this.onGetApiData()
-        //   }, 30000)
-        // })
+        .finally(() => {
+          setTimeout(() => {
+            this.onGetApiData()
+          }, 30000)
+        })
+    },
+
+    onGetHistoryCryptoPrice (cryptoName, historyPrice) {
+      this.$axios({ method: 'get', url: `https://api.coingecko.com/api/v3/coins/${cryptoName.toLowerCase()}/history?date=${historyPrice}&localization=false` })
+        .then((response) => {
+          if (!response || !response.data || !response.data.market_data || (response && response.data && !response.data.market_data)) {
+            console.log('Data inválida')
+            this.changeToNull()
+            return
+          }
+          this.price = response.data.market_data.current_price.usd.toFixed(2)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+
+    changeToNull () {
+      this.price = null
+    },
+
+    clearWhenExpand () {
+      this.changeToNull()
+      this.cryptosDatas.historyPrice = null
     }
   }
 }
